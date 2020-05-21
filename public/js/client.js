41cc890d78c471aef84efc8d5e037efddddf81a3
yeugautruc <yeugautruc@gmail.com>
2020-05-21 22:32:21 +0200

    update

--- public/js/client.js
+++ public/js/client.js
@@ -1,20 +1,74 @@
 
-const socket = io.connect('http://localhost:3000');
-const msgContainer = document.getElementById('chatbox-container');
+const socket = io.connect('localhost:3000');
+const msgContainer = document.getElementById('msg-container');
 const msgForm = document.getElementById('chatbox-form');
 const msgInput = document.getElementById('msgInput');
-socket.on('chat-msg', (data) => {
-    displayMsg(data);
+const helloID = document.getElementById('hello-id');
+const name = prompt("enter your name");
+displayMsgClient("You joined");
+window.scrollTo(0, 900);
+socket.emit('new-user', name);
+helloID.innerHTML = "Hello " + name;
+socket.on('user-exist', name => {
+    const newname = prompt("name has already existed, choose new one:");
+    socket.emit('new-user', newname);
+    helloID.innerHTML = "Hello " + newname;
+})
+socket.on('ask-msg', (data) => {
+    displayMsgServer(data.name + " asked about corona statisic of " + data.message + "!");
+    window.scrollTo(0, 900);
 });
+socket.on('info-corona', (data) => {
+    if (data === null) {
+        displayMsgServer("We don't have information about this city");
+        window.scrollTo(0, 900);
+    } else {
+        displayMsgServer("Base on information of Corona REST API:")
+        displayMsgServer("Name: " + data.name);
+        displayMsgServer("Bundesland: " + data.bl);
+        displayMsgServer("Cases: " + data.cases);
+        displayMsgServer("Deaths: " + data.deaths);
+        displayMsgServer("Death rate: " + Math.round(data.death_rate * 100) / 100 + "%");
+        displayMsgServer("Last update: " + data.last_update);
+        window.scrollTo(0, 900);
+    }
+});
+socket.on('chat-msg', data => {
+    displayMsgServer(data.name+ " said: " + data.message);
+    window.scrollTo(0, 900);
+})
+socket.on('user-connect', name => {
+    displayMsgServer(name + " is connected");
+    window.scrollTo(0, 900);
+})
 msgForm.addEventListener('submit', e => {
     e.preventDefault();
-    const msg = msgInput.value;
-    console.log(msg);
+    let msg = msgInput.value;
+    if (msg.includes("About")) {
+        outmsg = msg.replace("About ","");
+        displayMsgClient("You asked about corona statisic of " + outmsg);
+        window.scrollTo(0, 900);
+    } else{
+        displayMsgClient("You said: " + msg);
+        window.scrollTo(0, 900);
+    }
     socket.emit('send-msg', msg);
     msgInput.value = '';
 });
-function displayMsg(msg){
+function displayMsgClient(msg) {
     const msgElement = document.createElement('div');
-    msgElement.innerText=msg;
+    msgElement.setAttribute("style", "background-color: white;");
+    msgElement.innerText = msg;
     msgContainer.append(msgElement);
+}
+function displayMsgServer(msg) {
+    const msgElement = document.createElement('div');
+    msgElement.setAttribute("style", "background-color: orange; margin-left:auto; margin-right:5px;");
+    msgElement.innerText = msg;
+    msgContainer.append(msgElement);
+}
+function clearChat() {
+    while (msgContainer.hasChildNodes()) {
+        msgContainer.removeChild(msgContainer.firstChild);
+    }
 }
--- src/app.js
+++ src/app.js
@@ -1,19 +1,45 @@
+const fetch = require('node-fetch');
 const path = require('path');
 const express = require('express');
 const app = express();
-const publicPath = path.join(__dirname,'../public');
+const publicPath = path.join(__dirname, '../public');
 app.use(express.static(publicPath));
-const server = require('http').Server(app);
+const server = require('http').createServer(app);
 const io = require('socket.io')(server);
-
+let user = {};
 app.get('/', (req, res) => {
-  res.sendFile(__dirname+'/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
-socket.on('send-msg',msg=>{
-    socket.broadcast.emit('chat-msg',msg);
-})
+    console.log(socket.id + " connected")
+    socket.on('new-user', name => {
+        let userExist = (JSON.stringify(user).includes(name));
+        if (userExist) {
+            socket.emit('user-exist', name);
+        }
+        else {
+            user[socket.id] = name;
+            socket.broadcast.emit('user-connect', name);
+        }
+    })
+    socket.on('send-msg', msg => {
+        if (msg.includes("About")) {
+            fetch('http://141.41.235.21:1880/corona_regions/')
+                .then(response => response.json())
+                .then(data => {
+                    msg = msg.replace("About ","");
+                    console.log(msg);
+                    socket.broadcast.emit('ask-msg', { message: msg, name: user[socket.id] });
+                    socket.broadcast.emit('info-corona', data.data[msg]);
+                    socket.emit('info-corona', data.data[msg]);
+                })
+                .catch(err => { })
+        } else {
+            socket.broadcast.emit('chat-msg', { message: msg, name: user[socket.id] });
+        }
+    })
+
 });
 
 server.listen(3000);
